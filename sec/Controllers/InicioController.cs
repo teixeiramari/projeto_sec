a641154dbb011da3138e42a7233586ffd36ce498
Mari <mari.ane.teixeira.c@gmail.com>
2018-11-26 22:05:59 -0200

    .

--- sec/Controllers/InicioController.cs
+++ sec/Controllers/InicioController.cs
@@ -15,10 +15,34 @@ namespace sec.Controllers
         [Authorize]
         public ActionResult Index()
         {
-  
+            ViewBag.preferencias = db.Preferencias.ToList();
             return View(RetornaUsuario());
         }
-
+        public ActionResult AdicionarReferencia(int [] preferencias)
+        {
+            var usu = RetornaUsuario();
+            foreach(int pref in preferencias)
+            {
+                usu.Preferencias.Add(db.Preferencias.Find(pref));
+            }
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult CadastrarPref(string desc)
+        {
+            var usu = RetornaUsuario();
+            usu.Preferencias.Add(new Preferencia { Descricao = desc });
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        [HttpPost]
+        public JsonResult ExcluirPrefencia(int pref)
+        {
+            var usu = RetornaUsuario();
+            usu.Preferencias.Remove(db.Preferencias.Find(pref));
+            db.SaveChanges();
+            return Json("foi");
+        }
         public Usuario RetornaUsuario()
         {
             var identity = User.Identity as ClaimsIdentity;
---
+++ sec/ScriptSEC/Inicio.js
@@ -0,0 +1,22 @@
+﻿$('#anexarPref').on('click', function () {
+    $('#modalPreferencia').modal('show');
+});
+$('#addPref').on('click', function () {
+    $('#AdicionarPref').modal('show');
+});
+$('.close-pref').on('click', function () {
+    var pref = parseInt($(this).attr('ref'));
+    $(this).parent().remove();
+    $.ajax({
+        type: 'POST',
+        url: baseUrl + 'Inicio/ExcluirPrefencia',
+        contentType: 'application/json; charset=utf-8',
+        dataType: 'json',
+        data: JSON.stringify({ "pref": pref }),
+        success: function (data) {
+          
+            console.log(data);
+        },
+        error: function (error) { console.log(error); }
+    }).fail(function (error) { console.log(error); });
+});
