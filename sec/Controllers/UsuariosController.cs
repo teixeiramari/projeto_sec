c375bfcc74ef5dd40b11e20301fb2839245ffe87
gabbade <gabbade@gmail.com>
2018-11-30 01:06:42 -0200

    pagina usuario feita

---
+++ sec/Controllers/UsuariosController.cs
@@ -0,0 +1,35 @@
+﻿using sec.Models;
+using sec.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace sec.Controllers
+{
+    public class UsuariosController : Controller
+    {
+        private SecContext db = new SecContext();
+        // GET: Usuarios
+        public ActionResult Index()
+        {
+            var identity = User.Identity as ClaimsIdentity;
+            int id = Convert.ToInt32(identity.Claims.FirstOrDefault(c => c.Type == "Id").Value);
+            var usuario = db.Usuarios.Find(id);
+
+            List<int> zotro = new List<int>();
+            zotro.Add(id);
+            zotro.AddRange(usuario.Amigos.Select(u => u.Id));
+            MeuTodosUsuarios retorno = new MeuTodosUsuarios();
+            retorno.Amigos = usuario.Amigos;
+
+
+            retorno.Outros = db.Usuarios.Where(u => !zotro.Contains(u.Id)).ToList();
+            return View(retorno);
+
+        }
+    }
+}
---
+++ sec/ViewModels/MeuTodosUsuarios.cs
@@ -0,0 +1,19 @@
+﻿using sec.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace sec.ViewModels
+{
+    public class MeuTodosUsuarios
+    {
+        public MeuTodosUsuarios()
+        {
+            Amigos = new List<Usuario>();
+            Outros = new List<Usuario>();
+        }
+        public List<Usuario> Amigos { get; set; }
+        public List<Usuario> Outros { get; set; }
+    }
+}
